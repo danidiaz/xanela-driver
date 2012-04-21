62caa9c7f147f8399b9d3ba2328402e252feff3d
Daniel Diaz <diaz.carrete@facebook.com>
2012-04-21 16:08:32 +0200

    added trivial server impl

--- src/main/java/info/danidiaz/xanela/driver/Driver.java
+++ src/main/java/info/danidiaz/xanela/driver/Driver.java
@@ -1,9 +1,40 @@
 package info.danidiaz.xanela.driver;
 
+import java.io.IOException;
+
+import org.msgpack.rpc.Server;
+import org.msgpack.rpc.loop.EventLoop;
 
 public class Driver 
 {
     public static void premain(String agentArgs) {
         System.out.println( "Hi, I'm the agent!" );
+        
+        try {            
+            final EventLoop loop = EventLoop.defaultEventLoop();
+            final Server svr = new Server();
+            svr.serve(new Driver());
+            svr.listen(1985);      
+                        
+            new Thread(new Runnable() {
+                
+                @Override
+                public void run() {
+                    try {           
+                        loop.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+            
+    }
+    
+    public String hello(String msg, int a) {
+        return "foo";
     }
 }
