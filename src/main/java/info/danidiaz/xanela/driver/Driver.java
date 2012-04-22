c4fdf5fa415b89f494dadeccd6c365c4ed2022af
Daniel Diaz <diaz.carrete@facebook.com>
2012-04-22 16:52:51 +0200

    ability to change the default listening port

--- src/main/java/info/danidiaz/xanela/driver/Driver.java
+++ src/main/java/info/danidiaz/xanela/driver/Driver.java
@@ -7,15 +7,24 @@ import org.msgpack.rpc.loop.EventLoop;
 
 public class Driver 
 {
+    
+    // http://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xml
+    private static int DEFAULT_PORT = 26060;
+    
+    // http://docs.oracle.com/javase/6/docs/api/java/lang/instrument/package-summary.html
     public static void premain(String agentArgs) {
         System.out.println( "Hi, I'm the agent, started with options: " + agentArgs );
-        
+                
         try {            
             final EventLoop loop = EventLoop.defaultEventLoop();
             final Server svr = new Server();
             svr.serve(new Driver());
-            // http://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xml
-            svr.listen(26060);      
+
+            int port = DEFAULT_PORT;
+            if (!agentArgs.isEmpty()) {
+                port = Integer.decode(agentArgs);
+            } 
+            svr.listen(port);      
                         
             new Thread(new Runnable() {
                 
@@ -29,7 +38,10 @@ public class Driver
                     
                 }
             }).start();
-        } catch (IOException e) {
+            System.out.println("Xanela server started at port " + port);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }catch (IOException e) {       
             e.printStackTrace();
         }
             
