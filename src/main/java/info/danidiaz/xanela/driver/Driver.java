4515e5444daed91106583a04ec23a93542a8ba34
Daniel Diaz <diaz.carrete@facebook.com>
2012-04-26 00:38:18 +0200

    packer write operations are now carried out inside de EDT

--- src/main/java/info/danidiaz/xanela/driver/Driver.java
+++ src/main/java/info/danidiaz/xanela/driver/Driver.java
@@ -2,6 +2,9 @@ package info.danidiaz.xanela.driver;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
 
 import org.msgpack.MessagePackable;
 import org.msgpack.MessageTypeException;
@@ -58,13 +61,30 @@ public class Driver
     static class XanelaObject implements MessagePackable {
 
         @Override
-        public void messagePack(Packer packer) throws IOException { 
-            packer.packArray(2);
-            packer.pack("fim fam fum");
-            packer.packArray(3);
-            packer.pack((int)1);
-            packer.pack((int)2);
-            packer.pack((int)3);
+        public void messagePack(final Packer packer) throws IOException {
+            
+            try {
+                SwingUtilities.invokeAndWait(new Runnable() {
+                    
+                    @Override
+                    public void run() {
+                        try {
+                            packer.packArray(2);
+                            packer.pack("fim fam fum");                            
+                            packer.packArray(3);
+                            packer.pack((int)1);
+                            packer.pack((int)2);
+                            packer.pack((int)3);                                                    
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }            
         }
         
     }
@@ -77,7 +97,4 @@ public class Driver
         }        
     }
     
-    public String hello(String msg, int a) {
-        return "foo";
-    }
 }
