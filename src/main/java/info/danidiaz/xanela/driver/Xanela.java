6dccc329f6ada7468298b0306fb60c58a748ab20
Daniel Diaz <diaz.carrete@facebook.com>
2012-05-05 23:28:24 +0200

    the containment hierarchy is now exported

--- src/main/java/info/danidiaz/xanela/driver/Xanela.java
+++ src/main/java/info/danidiaz/xanela/driver/Xanela.java
@@ -1,11 +1,14 @@
 package info.danidiaz.xanela.driver;
 
+import java.awt.Component;
 import java.awt.Window;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.RootPaneContainer;
 import javax.swing.SwingUtilities;
 
 import org.msgpack.MessagePackable;
@@ -22,29 +25,7 @@ public class Xanela implements MessagePackable {
                 public void run() {
                     try {
                         Window warray[] = Window.getOwnerlessWindows();
-                        int visibleCount = 0;
-                        for (int i=0;i<warray.length;i++) {                            
-                            if (warray[i].isVisible()) {
-                                visibleCount++;
-                            }
-                        }
-                        packer.packArray(visibleCount);
-                        for (int i=0;i<warray.length;i++) {
-                            Window w = warray[i];
-                            if (warray[i].isVisible()) {
-                                String title = "";
-                                if (w instanceof JFrame) {
-                                    title = ((JFrame)w).getTitle();
-                                } else if (w instanceof JDialog) {
-                                    title = ((JDialog)w).getTitle();                                    
-                                }
-                                packer.packArray(2);
-                                packer.pack(title);
-                                packer.packArray(2);
-                                packer.pack((int)w.getHeight());
-                                packer.pack((int)w.getWidth());
-                            }
-                        }                                                                                                   
+                        writeWindowArray(packer, warray);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -57,4 +38,64 @@ public class Xanela implements MessagePackable {
         }            
     }
     
+    private static int countVisible(Component[] warray) {
+        int visibleCount = 0;
+        for (int i=0;i<warray.length;i++) {                            
+            if (warray[i].isVisible()) {
+                visibleCount++;
+            }
+        }
+        return visibleCount;
+    }
+    
+    private static void writeWindowArray(Packer packer, Window warray[]) throws IOException {
+        packer.packArray(countVisible(warray));
+        for (int i=0;i<warray.length;i++) {
+            Window w = warray[i];
+            if (warray[i].isVisible()) {
+                String title = "";
+                if (w instanceof JFrame) {
+                    title = ((JFrame)w).getTitle();
+                } else if (w instanceof JDialog) {
+                    title = ((JDialog)w).getTitle();                                    
+                }
+                packer.packArray(4);
+                packer.pack(title);
+                packer.packArray(2);
+                packer.pack((int)w.getHeight());
+                packer.pack((int)w.getWidth());
+                
+                RootPaneContainer rpc = (RootPaneContainer)w;
+                writeComponent(packer, rpc.getContentPane());
+                
+                if (w instanceof JFrame) {
+                    System.out.println(((JFrame)w).getContentPane().getClass().getName());
+                } else if (w instanceof JDialog) {
+                    System.out.println(((JDialog)w).getContentPane().getClass().getName());                                   
+                }                
+                writeWindowArray(packer, w.getOwnedWindows());
+            }        
+        }
+    }
+    
+    private static void writeComponent(Packer packer, Component c) throws IOException {
+        JComponent jc = (JComponent) c;
+        
+        packer.packArray(3);
+        
+        packer.pack(c.getClass().getName());
+        
+        packer.packArray(2);
+        packer.pack((int)c.getHeight());
+        packer.pack((int)c.getWidth());
+        
+        Component children[] = jc.getComponents();
+        packer.packArray(countVisible(children));
+        for (int i=0;i<children.length;i++) {
+            if (children[i].isVisible()) {
+                writeComponent(packer, children[i]);
+            }
+        }
+    }
+
 }
